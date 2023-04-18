7c58e0f6a705e595937ed49439180f69080e7465
tisteryu <tisteryu@tencent.com>
2023-04-18 20:44:19 +0800

    DOCKER_BUILDKIT=0

--- cmd/build_image.sh
+++ cmd/build_image.sh
@@ -1,1 +1,1 @@
-docker build -t mirrors.tencent.com/rms/llama_finetune:v2 . -f docker/Dockerfile.lora-finetune
+DOCKER_BUILDKIT=0 docker build -t mirrors.tencent.com/rms/llama_finetune:v2 . -f docker/Dockerfile.lora-finetune
