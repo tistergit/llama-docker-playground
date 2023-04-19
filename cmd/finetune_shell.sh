9d71c700268a39f16d943e671424dcef8c96e1ed
tisteryu <tisteryu@tencent.com>
2023-04-19 08:23:46 +0800

    返回旧的pytorch基础镜像

--- cmd/finetune_shell.sh
+++ cmd/finetune_shell.sh
@@ -2,4 +2,5 @@ docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     --rm -it \
     -v `pwd`/models:/app/alpaca-lora/original-weights \
     -v `pwd`/weights:/app/alpaca-lora/weights \
+    -v `pwd`/samples:/app/alpaca-lora/samples \
     mirrors.tencent.com/rms/llama_finetune:v2 bash
