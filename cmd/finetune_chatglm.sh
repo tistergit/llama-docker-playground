2d065b497689539769707fc518e96097458e65ae
tisteryu <tisteryu@tencent.com>
2023-04-22 13:01:59 +0800

    link samples to docker

--- cmd/finetune_chatglm.sh
+++ cmd/finetune_chatglm.sh
@@ -3,4 +3,5 @@
 docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p 8080:7860 \
     --rm -it \
     -v `pwd`/models/chatglm-6b:/app/ChatGLM-6B/THUDM/chatglm-6b \
+    -v `pwd`/samples:/app/ChatGLM-6B/samples \
     mirrors.tencent.com/rms/llama_finetune:chatglm /bin/bash
