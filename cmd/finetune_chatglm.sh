4e82bddd974b517946dd2d09549bcb6aec224af9
tisteryu <tisteryu@tencent.com>
2023-04-23 09:02:22 +0800

    fix chatglm

--- cmd/finetune_chatglm.sh
+++ cmd/finetune_chatglm.sh
@@ -3,5 +3,5 @@
 docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p 8080:7860 \
     --rm -it \
     -v `pwd`/models/chatglm-6b:/app/ChatGLM-6B/ptuning/THUDM/chatglm-6b \
-    -v `pwd`/samples:/app/ChatGLM-6B/ptuning/AdvertiseGen \
+    -v `pwd`/samples:/app/ChatGLM-6B/ptuning \
     mirrors.tencent.com/rms/llama_finetune:chatglm /bin/bash
