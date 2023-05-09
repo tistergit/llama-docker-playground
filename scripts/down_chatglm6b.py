ae4eeecc92b8fc4eeaaff25a8a24c3b7821a021a
tisteryu <tisteryu@tencent.com>
2023-05-09 15:17:07 +0800

    fix 6b model download

--- scripts/down_chatglm6b.py
+++ scripts/down_chatglm6b.py
@@ -1,5 +1,5 @@
 from huggingface_hub import snapshot_download
 import os
-repos_id = 'THUDM/chatglm-130b' # original model 'THUDM/chatglm-6b' quantized 4bit 'THUDM/chatglm-6b-int4' 8bit 'THUDM/chatglm-6b-int8'
+repos_id = 'THUDM/chatglm-6b' # original model 'THUDM/chatglm-6b' quantized 4bit 'THUDM/chatglm-6b-int4' 8bit 'THUDM/chatglm-6b-int8'
 download_dir='./'+repos_id
 snapshot_download(repo_id=repos_id, local_dir=download_dir, repo_type='model')
