a3649746f3e2534343e95c01b7aa413a7c65ab32
tisteryu <tisteryu@tencent.com>
2023-05-16 18:15:20 +0800

    fix path

--- cmd/finetune_chatglm130b.sh
+++ cmd/finetune_chatglm130b.sh
@@ -3,4 +3,5 @@
 docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     --rm -it \
     -v `pwd`/models:/app/models \
+    -v `pwd`/models/chatglm-130b-8:/app/models/chatglm-130b \
     mirrors.tencent.com/rms/finetuning:chatglm130b /bin/bash
